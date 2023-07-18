aaf39210291e3609757297db5decdf63ad438dc2
Prado <peu_prado01@hotmail.com>
2023-07-18 06:05:32 -0300

    feat: cria API mongo

---
+++ controllers/pictureController.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const Picture = require("../models/Picture");
+
+exports.create = async (req, res) => {
+  try {
+    const { name, 
+      idade,
+      descricao,
+      uf,
+      cidade
+    } = req.body;
+
+    const file = req.file;
+    const picture = new Picture({
+      name,
+      foto: file.path,
+      idade,
+      descricao,
+      uf,
+      cidade
+    });
+
+    await picture.save();
+    res.json(picture);
+  } catch (err) {
+    res.status(500).json({ message: "Erro ao salvar a imagem." });
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const picture = await Picture.findById(req.params.id);
+    if (!picture) {
+      return res.status(404).json({ message: "Imagem não encontrada" });
+    }
+    fs.unlinkSync(picture.foto);
+    await picture.remove();
+    res.json({ message: "Imagem removida com sucesso" });
+  } catch (err) {
+    res.status(500).json({ message: "Erro ao remover a imagem" });
+  }
+};
+
+exports.findAll = async (req, res) => {
+  try {
+    const pictures = await Picture.find();
+    res.json(pictures);
+  } catch (err) {
+    res.status(500).json({ message: "Erro ao buscar as imagens." });
+  }
+};
---
+++ models/Picture.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PictureSchema = new Schema({
+  name: { type: String, required: true },
+  foto: { type: String, required: true },
+  idade: {type: String, required: true},
+  descricao: {type: String, required: true},
+  uf: {type: String, required: true},
+  cidade: {type: String, required: true},
+});
+
+module.exports = mongoose.model("Picture", PictureSchema);
