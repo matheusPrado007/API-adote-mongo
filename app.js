4570c46ee00d34b771477241ed69b3c42e5c7b63
Prado <peu_prado01@hotmail.com>
2023-07-21 09:21:12 -0300

    feat: permissoes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 require("./db");
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://adotecomamor.netlify.app/index.html#', 'http://127.0.0.1:5500/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
