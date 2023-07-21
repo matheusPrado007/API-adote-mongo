7c04727eb00ac6b2df68e6a3053caca4b8065f18
Prado <peu_prado01@hotmail.com>
2023-07-21 09:16:42 -0300

    feat: permissoes

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.use('/uploads', express.static('uploads'));
 require("dotenv").config();
 require("./db");
 
-app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://adotecomamor.netlify.app/index.html#', 'http://127.0.0.1:5500/');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+app.use(cors());
 
 
 const port = process.env.PORT || 3000;
